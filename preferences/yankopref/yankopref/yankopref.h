dd4e75cb5a854984721fd824c20ac52d65a3ef3a
cacko <alex@cacko.net>
2022-08-06 22:54:51 +0100

    diaaaaaazeeeeeeeeeepam

---
+++ preferences/yankopref/yankopref/yankopref.h
@@ -0,0 +1,14 @@
+//
+//  yankopref.h
+//  yankopref
+//
+//  Created by Alex on 06/08/2022.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface yankopref : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ preferences/yankopref/yankopref/yankopref.m
@@ -0,0 +1,16 @@
+//
+//  yankopref.m
+//  yankopref
+//
+//  Created by Alex on 06/08/2022.
+//
+
+#import "yankopref.h"
+
+@implementation yankopref
+
+- (void)mainViewDidLoad
+{
+}
+
+@end

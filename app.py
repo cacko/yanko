f60bc9e3db681c1ac24140f13bea54c77eba3f4a
cacko <alex@cacko.net>
2022-08-06 18:02:59 +0100

    Git wants e to commit, I want to sleep. Take me sweet void.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from yanko.cli import cli
-from yanko import start
 from sys import argv
 
 if len(argv) > 1:
+    from yanko.cli import cli
     cli()
 else:
+    from yanko import start
     start()

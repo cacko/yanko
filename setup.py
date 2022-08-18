ba000873bfea65ebbe4a8d71e5f84b275291f390
cacko <alex@cacko.net>
2022-08-18 15:12:04 +0100

    assorted changes

--- setup.py
+++ setup.py
@@ -1,35 +1,4 @@
-from setuptools import setup, find_packages
-
-
-
-# setup(
-#     name=__name__,
-#     version=__version__,
-#     author='cacko',
-#     author_email='cacko@cacko.net',
-#     distclass=Distribution,
-#     url='http://pypi.cacko.net/simple/tick/',
-#     description='whatever',
-#     install_requires=[
-#         "click >= 8.0.4",
-#         "toml >= 0.10.2",
-#         "dataclasses-json >= 0.5.6",
-#         "stringcase >= 1.2.0",
-#         "requests >= 2.27.1",
-#         "questionary >= 1.10.0",
-#         "pyfiglet >= 0.0.post1",
-#         "appdir >= 0.2",
-#         "PyYAML >= ^6.0",
-
-#     ],
-#     setup_requires=['wheel'],
-#     python_requires=">=3.10",
-#     packages=find_packages(include=['tick', 'tick.*']),
-#     entry_points="""
-#         [console_scripts]
-#         tick=tick.cli:cli
-#     """,
-# )
+from setuptools import setup
 
 
 APP = ['app.py']
@@ -42,38 +11,33 @@ OPTIONS = {
         'CFBundleIdentifier': 'net.cacko.yanko',
         'CFBundleVersion': '0.2.0'
     },
+    'packages': [
+        '_sounddevice_data',
+    ],
 }
 setup(
     app=APP,
     name="Yanko",
-    data_files=DATA_FILES,
     install_requires=[
-        "pyfiglet >= 0.8.post1",
-        "PyGObject >= 3.42.1",
-        "requests >= 2.28.1",
-        "packaging >= 21.3",
-        "PyYAML >= 6.0",
-        "py2app >= 0.28.2",
-        "toml >= 0.10.2",
-        "tld >= 0.12.6",
-        "arrow >= 1.2.2",
-        "dataclasses-json >= 0.5.7",
-        "rumps >= 0.5.9",
-        "bottle >= 0.12.21",
-        "Pillow >= 9.2.0",
-        "appdir >= 0.2",
-        "pyotp >= 2.6.0",
-        "click >= 8.1.3",
-        "pixelme >= 0.4.7",
-        "bs4 >= 0.0.1",
-        "miniaudio >= 1.52",
-        "ffmpeg-python >= 0.2.0",
-        "numpy >= 1.23.2",
-        "sounddevice >= 0.4.4",
-        "pycparser",
-        "cffi"
+        'rumps >= 0.5.9',
+        'dataclasses_json >= 0.5.7',
+        'requests >= 2.28.1',
+        'tld >= 0.12.6',
+        'pillow >= 9.2.0',
+        'appdir >= 0.2',
+        'pyotp >= 2.6.0',
+        'click >= 8.1.3',
+        'pixelme>= 0.4.7',
+        'opencv-python >= 4.6.0.66',
+        'bs4 >= 0.0.1',
+        'miniaudio',
+        'ffmpeg-python>=0.2.0',
+        'numpy>=1.23.2',
+        'sounddevice>=0.4.4',
+        'cffi >= 1.15.1',
+        'pycparser >= 2.21'
     ],
-    packages=find_packages(include=['yanko', 'yanko.*', 'sounddevice.*']),
+    data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )

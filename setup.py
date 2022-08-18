047b1e75c7c743dea5a45f4527aa5f7fbb9b6cbc
cacko <alex@cacko.net>
2022-08-18 15:51:15 +0100

    Derp, asset redirection in dev mode

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ OPTIONS = {
     'plist': {
         'LSUIElement': True,
         'CFBundleIdentifier': 'net.cacko.yanko',
-        'CFBundleVersion': '0.2.0'
+        'CFBundleVersion': '0.2.0',
+        'LSEnvironment': dict(
+            PATH='/Users/jago/.rbenv/shims:/Users/jago/.rbenv/bin:/Users/jago/.local/bin:/usr/local/sbin:/Users/jago/.pyenv/shims:/Users/jago/bin:/usr/local/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/opt/homebrew/bin:/opt/local/bin:/opt/local/sbin:~/.local/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Applications/VMware Fusion.app/Contents/Public:/usr/local/go/bin:/usr/local/MacGPG2/bin:/usr/local/opt/llvm/bin:/Library/Apple/usr/bin:/Applications/Wireshark.app/Contents/MacOS:/Users/jago/.pyenv/libexec:/Users/jago/.cargo/bin:/Users/jago/Library/Android/sdk/platform-tools',
+        )
     },
     'packages': [
         '_sounddevice_data',

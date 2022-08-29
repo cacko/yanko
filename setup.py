48d8eb322a28eae13481547aad97eea2f3f64fe3
cacko <alex@cacko.net>
2022-08-29 12:29:21 +0100

    hoo boy

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@ def resolve_libs(libs):
 
 
 def version():
-    print(sys.argv)
-    if len(sys.argv) > 1 and sys.argv[1] == "bdist_wheel":
+    if len(sys.argv) > 1 and sys.argv[1] == "py2app":
         init = Path(__file__).parent / __name__.lower() / "version.py"
         _, v = init.read_text().split(' = ')
         cv = semver.VersionInfo.parse(v.strip('"'))

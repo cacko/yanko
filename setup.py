4161e8444bca12e69767f08cee8394c4407fac1a
cacko <alex@cacko.net>
2023-09-13 18:51:32 +0100

    lolwhat?

--- setup.py
+++ setup.py
@@ -108,7 +108,8 @@ setup(
         "corefile>=0.1.2",
         "librosa>=0.10.0",
         "quickmachotkey>=2023.3.4",
-        "pyperclip3>=0.4.1"
+        "pyperclip3>=0.4.1",
+        "pyobjc-framework-Cocoa"
     ],
     python_requires=">=3.10",
     data_files=DATA_FILES,
